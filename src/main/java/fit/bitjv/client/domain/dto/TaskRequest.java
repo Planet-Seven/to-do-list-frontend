3d0ed372c6b88aef017bb9f0037f9f17b898c6e8
Vojtech Svoboda <svobov25@fit.cvut.cz>
2024-12-17 18:05:08 +0100

    fix deadline input

--- src/main/java/fit/bitjv/client/domain/dto/TaskRequest.java
+++ src/main/java/fit/bitjv/client/domain/dto/TaskRequest.java
@@ -3,6 +3,7 @@ package fit.bitjv.client.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,7 +12,7 @@ import java.util.Set;
 @Setter
 public class TaskRequest {
     private String name;
-    private Date deadline;
+    private String deadline;
     private boolean isDone;
     private Long categoryId;
     private Set<Long> tagIds = new HashSet<>();
--- src/main/java/fit/bitjv/client/domain/dto/TaskResponse.java
+++ src/main/java/fit/bitjv/client/domain/dto/TaskResponse.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
